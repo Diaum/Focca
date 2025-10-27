a1f805077a7eec52726b21c92de493e64e5d9d45
Horacio <contato@java10x.dev>
2025-10-27 20:58:49 -0300

    Initial Commit

---
+++ Focca/FoccaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoccaApp.swift
+//  Focca
+//
+//  Created by Fiasco on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FoccaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
