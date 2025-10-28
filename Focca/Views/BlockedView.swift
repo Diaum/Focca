506a94b035cffa8fde7a26ca9ddad5f8fa7b349d
Horacio <contato@java10x.dev>
2025-10-28 00:08:51 -0300

    Remove TabBar and TabItem components from BlockedView.swift to streamline the UI structure.

--- Focca/Views/BlockedView.swift
+++ Focca/Views/BlockedView.swift
@@ -78,54 +78,6 @@ struct BlockedView: View {
     }
 }
 
-// MARK: - Tab Bar
-
-struct TabBar: View {
-    @Binding var selectedTab: Int
-    
-    var body: some View {
-        HStack {
-            TabItem(title: "Brick", isSelected: selectedTab == 0)
-                .onTapGesture { selectedTab = 0 }
-            
-            TabItem(title: "Schedule", isSelected: selectedTab == 1)
-                .onTapGesture { selectedTab = 1 }
-            
-            TabItem(title: "Activity", isSelected: selectedTab == 2)
-                .onTapGesture { selectedTab = 2 }
-            
-            TabItem(title: "Settings", isSelected: selectedTab == 3)
-                .onTapGesture { selectedTab = 3 }
-        }
-        .frame(height: 60)
-        .background(Color(hex: "0F0F0F"))
-    }
-}
-
-struct TabItem: View {
-    let title: String
-    let isSelected: Bool
-    
-    var body: some View {
-        VStack(spacing: 4) {
-            Text(title)
-                .font(.system(size: 12, weight: .regular))
-                .foregroundColor(isSelected ? .white : Color(hex: "8E8E93"))
-            
-            if isSelected {
-                Circle()
-                    .fill(Color.white)
-                    .frame(width: 4, height: 4)
-            } else {
-                Circle()
-                    .fill(Color.clear)
-                    .frame(width: 4, height: 4)
-            }
-        }
-        .frame(maxWidth: .infinity)
-    }
-}
-
 #Preview {
     BlockedView()
 }
